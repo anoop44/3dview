4d92173f258f267bf9e556ac536b2e933950da24
Anoop <anoopvvs@gmail.com>
2020-03-04 10:57:11 +0530

    Artifact publishing added

--- 3dview/build.gradle.kts
+++ 3dview/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.novoda.gradle.release.PublishExtension
+
 plugins {
     id("com.android.library")
     kotlin("android")
+    id("com.novoda.bintray-release")
 }
+
 
 android {
     compileSdkVersion(29)
@@ -9,8 +13,8 @@ android {
     defaultConfig {
         minSdkVersion(14)
         targetSdkVersion(29)
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = rootProject.extra["versionCode"] as Int
+        versionName = rootProject.extra["versionName"] as String
     }
 
     buildTypes {
@@ -27,4 +31,13 @@ android {
 
 dependencies {
     implementation(kotlin("stdlib-jdk7", rootProject.extra["kotlinVersion"] as String ))
+}
+
+configure<PublishExtension> {
+    userOrg = "anoop44"
+    groupId = "ss.anoop"
+    artifactId = "3dview"
+    publishVersion = rootProject.extra["versionName"] as String
+    desc = "A simple ViewGroup that gives any view a 3d appearance"
+    website = "https://github.com/anoop44/3dview"
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,21 @@
 buildscript {
     var kotlinVersion:String by extra
     kotlinVersion = "1.3.61"
+
+    var versionName: String by extra
+    versionName = "1.0.0"
+
+    var versionCode: Int by extra
+    versionCode = 100
+
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0-alpha01")
+        classpath("com.android.tools.build:gradle:3.5.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-
+        classpath("com.novoda:bintray-release:0.9.2")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
